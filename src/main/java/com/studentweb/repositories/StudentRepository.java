a6cf9872bb8855251ceabe46fd90a92e0ada858d
Samiksha Mehendiratta <kapoormisha52@gmail.com>
2023-05-13 22:53:32 +0530

    remove  files

--- src/main/java/com/studentweb/repositories/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.studentweb.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.studentweb.entities.Students;
-
-public interface StudentRepository extends JpaRepository<Students, Long> {
-
-}
